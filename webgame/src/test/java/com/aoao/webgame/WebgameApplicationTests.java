6d6c0f8867936db585fe7f96eb38683224efed84
a.nishikawa <a.nishikawa@nagatake.co.jp>
2026-03-13 11:48:54 +0900

    Add workspace_study including webgame

---
+++ webgame/src/test/java/com/aoao/webgame/WebgameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aoao.webgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebgameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
